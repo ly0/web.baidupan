d3c814ae8e4287cbf7351c02663e92afdadc31ca
latyas@gmail.com <latyas@gmail.com>
2014-08-28 04:08:52 +0800

    first uploaded

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 web.baidupan
 ============
 
-百度云网盘web端
+纯属娱乐, 代码丑陋功能简略, javascript什么的毛也不会, 离线任务懒得弄了, 可以把模板复制粘贴一份调用*baidupcsapi*的接口就好.
+
+截图:
+![image](http://github.com/ly0/web.baidupan/screenshot.png)
+
+运行:<br>
+`python launcher.py`
+
+依赖: <br>
+[baidupcsapi](http://github.com/ly0/baidupcsapi)
+[web.py](http://webpy.org)
---
+++ launcher.py
@@ -0,0 +1,70 @@
+#coding=utf-8
+import json
+import os
+import web
+import time
+from baidupcsapi import *
+
+# urls support regular expression
+# request will be sent with arguments of re's groups
+
+urls = (
+    '/', 'Index',
+    '/(js)/(.*)', 'Resource',
+    '/(css)/(.*)', 'Resource',
+    '/download/(.*)', 'Download',
+    '/file/(.*)', 'File'
+)
+
+pcs = PCS('username', 'password')
+
+
+def self_round(n, d):
+    return round(n, d)
+
+
+class Index:
+    def GET(self, *args):
+        # return pcs.list_files('/').content
+        render = web.template.render('templates')
+        data = {'pcs': json.loads(pcs.list_files('/').content)['list'],
+                'quota': json.loads(pcs.quota().content),
+                'pcsobj': pcs,
+                'modules': {'time': time,
+                            'round': round}
+                }
+        return render.list_files(data=data)
+
+
+class File:
+    def GET(self, *args):
+        # return pcs.list_files('/').content
+        render = web.template.render('templates')
+        data = {'pcs': json.loads(pcs.list_files('/' + '/'.join(args)).content)['list'],
+                'quota': json.loads(pcs.quota().content),
+                'pcsobj': pcs,
+                'modules': {'time': time,
+                            'round': round}
+                }
+        return render.list_files(data=data)
+
+
+class Resource:
+    def GET(self, *args):
+        path = os.path.join('./static', '/'.join(args))
+        if not os.path.exists(path):
+            return web.notfound('Resource not found.')
+
+        with open(path, 'r') as fp:
+            content = fp.read()
+        return content
+
+
+class Download:
+    def GET(self, *args):
+        path = '/' + '/'.join(args)
+        return pcs.download_url(path)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
