317fab5a540ff61bf510f85cb381d0b7a2cd4f31
Yangjie Li <latyas@gmail.com>
2014-08-29 06:07:55 +0800

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -63,7 +63,7 @@ class Resource:
 class Download:
     def GET(self, *args):
         path = '/' + '/'.join(args)
-        return web.redirect(pcs.download_url(path)[0])
+        return pcs.download_url(path)[0]
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
